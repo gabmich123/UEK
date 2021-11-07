5fae74dc0c857819d39df3bacb3f4fab77bf43fa
gabmich123 <93841017+gabmich123@users.noreply.github.com>
2021-11-07 11:21:32 +0100

    Add files via upload

---
+++ 4.10.py
@@ -0,0 +1,6 @@
+def read_number():
+    N=int(input("Enter number: "))
+    return N
+num1=read_number()
+num2=read_number()
+print(num1+num2)
---
+++ 4.12.py
@@ -0,0 +1,3 @@
+def sum(N):
+    for i in range(1, n+1):
+        print(sum(n))
