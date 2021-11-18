38ef4138f59d28cee5838956402997b4a55816c7
gabmich123 <93841017+gabmich123@users.noreply.github.com>
2021-11-19 00:54:44 +0100

    Delete 3.19.py

--- 3.19.py
+++
@@ -1,8 +0,0 @@
-x=int(input("Enter the dog's age in human years: "))
-z=0
-for y in range(1,x+1):
-    if y <=2:
-        z=z+10.5
-    else:
-        z=z+4
-print(f"The dog's age in dog’s years is {z} years.")
