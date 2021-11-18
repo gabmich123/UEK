d3a2b39288a87bcaa097437efff9b08775bd63db
gabmich123 <93841017+gabmich123@users.noreply.github.com>
2021-11-18 21:46:48 +0100

    Zadanie4

---
+++ zadanie4/4.16.py
@@ -0,0 +1,4 @@
+def month(n):
+    month=["Styczeń","Luty","Marzec","Kwiecień","Maj","Czerwiec","Lipiec","Sierpień","Lipiec","Sierpień","Wrzesień","Październik","Listopad","Grudzień"]
+    return month[n-1]
+print(month(7))
