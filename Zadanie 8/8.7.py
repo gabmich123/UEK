4972214adb5ffe49fb41d3c9a9550029dd1f091f
gabmich123 <93841017+gabmich123@users.noreply.github.com>
2021-12-11 10:01:13 +0100

    Add files via upload

---
+++ Zadanie 8/8.7.py
@@ -0,0 +1,8 @@
+person = {
+    'name': 'Gabriela',
+    'surname': 'Michałuszka',
+    'age': '22',
+    'sex': 'F',
+    'married': 'No',
+    'phone': {'private': '223874', 'public': '123654789'}
+    }
